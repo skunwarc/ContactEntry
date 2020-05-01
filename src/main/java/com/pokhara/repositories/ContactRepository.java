844279e4a3aebec33aa5f8b08e0a1b873691b7dc
skunwarcc <sagar.kunwar24@gmail.com>
2020-04-30 20:32:36 -0400

    modified repo class with jpa

--- src/main/java/com/pokhara/repositories/ContactRepository.java
+++ src/main/java/com/pokhara/repositories/ContactRepository.java
@@ -1,11 +1,12 @@
 package com.pokhara.repositories;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import com.pokhara.entities.ContactEntity;
 
 @Repository
-public interface ContactRepository extends CrudRepository<ContactEntity, Integer>{
+public interface ContactRepository extends JpaRepository<ContactEntity, Integer>{
 
 }
